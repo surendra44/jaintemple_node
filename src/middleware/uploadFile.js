c0a57e48a3e83dd127a0bcc7157110d90531f5de
Dixit <dixitsingh2023@gmail.com>
2023-09-11 15:24:05 +0530

    done

--- src/middleware/uploadFile.js
+++ src/middleware/uploadFile.js
@@ -4,7 +4,7 @@ import path from "path";
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'src/uploads/')
+        cb(null, 'uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now()+file.originalname)
